8f00449696e2302fd6738172ca7f3658b435e531
Corallana <kamillastar549@gmail.com>
2023-06-01 14:27:25 +0300

    3 commit

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -1,32 +1,40 @@
+
 import { BasketItem } from "./BasketItem";
 
 function BasketList(props) {
-    const { order = [], 
+    const {
+        order = [], 
         handleBasketShow = Function.prototype,
-        removeFromBasket = Function.prototype,
-        incQuantity = Function.prototype,
-        decQuantity = Function.prototype,
-} = props;
+        removeFromBasket =  Function.prototype,
+        incQuantity =  Function.prototype,
+        decQuantity =  Function.prototype,
+    } = props;
     const totalPrice = order.reduce((sum, el) => {
         return sum + el.price * el.quantity;
     }, 0);
+
     return (
         <ul className="collection basket-list">
             <li className="collection-item active indigo darken-4">Корзина</li>
             {order.length ? (
-                order.map((item) => 
-                <BasketItem 
-                key={item.id}
-                removeFromBasket={removeFromBasket}
-                incQantity={incQuantity}
-                decQantity={decQuantity}
-                {...item} />)
+                order.map((item) => (
+                    <BasketItem 
+                        key={item.id} 
+                        removeFromBasket={removeFromBasket}
+                        incQuantity={incQuantity}
+                        decQuantity={decQuantity} 
+                    {...item}
+                    />
+                ))
             ) : (
-            <li className="collection-item">Корзина пуста</li>
+                <li className="collection-item">Корзина пуста</li>
             )}
-            <li className="collection-item active indigo darken-4">Общая стоимость заказа: {totalPrice} руб. </li>
+            <li className="collection-item active indigo darken-4">
+                Общая стоимость заказа: {totalPrice} руб.
+            </li>
             <i className="material-icons basket-clear" onClick={handleBasketShow}>clear</i>
         </ul>
     );
 }
+
 export { BasketList };
